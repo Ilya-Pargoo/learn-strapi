b5410001da1a71ddf1113288784757db4f0f785e
IliaDigiWay <ilya.zabrotski@pargoo.com>
2024-11-28 10:35:41 +0100

    Added new service findOneBySlug and updated dream-story controller with custom logic

--- src/api/dream-story/controllers/dream-story.ts
+++ src/api/dream-story/controllers/dream-story.ts
@@ -3,11 +3,10 @@ import { factories } from '@strapi/strapi';
 export default factories.createCoreController('api::dream-story.dream-story', ({ strapi }) => ({
   async findOne(ctx) {
     const { id } = ctx.params;
-    
+    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+
     try {
-      const entity = await strapi.db.query('api::dream-story.dream-story').findOne({
-        where: { slug: id },
-      });
+      const entity = await strapi.service('api::dream-story.dream-story').findOneBySlug(id, sanitizedQueryParams);
 
       if (!entity) {
         return ctx.notFound('Dream story not found');
--- src/api/dream-story/services/dream-story.ts
+++ src/api/dream-story/services/dream-story.ts
@@ -4,4 +4,14 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::dream-story.dream-story');
+
+export default factories.createCoreService('api::dream-story.dream-story', ({ strapi }) => ({
+  async findOneBySlug(slug: string, query: Record<string, any>) {
+    const entities = await strapi.entityService.findMany('api::dream-story.dream-story', {
+      filters: { slug },
+      ...query,
+    });
+
+    return entities.length > 0 ? entities[0] : null;
+  },
+}));
